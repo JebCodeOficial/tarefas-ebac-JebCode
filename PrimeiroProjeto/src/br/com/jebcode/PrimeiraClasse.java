ef8d30c021061211a78d887cbeea795c9a5104d3
JebCodeOficial <jebcode@outlook.com.br>
2024-09-21 11:33:16 -0300

    Versão inicial do projeto PrimeiroProjeto no módulo 5

---
+++ PrimeiroProjeto/src/br/com/jebcode/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package br.com.jebcode;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Meu nome é Jonathan, mas sou conhecido pelo apelido Jeb");
+    }
+}
